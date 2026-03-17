58ffd4faa5c268b3d295c94e111c78957bc8a7b3
souldev003 <mehrabhosen003@gmail.com>
2026-03-17 16:56:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import DaisyNav from "./Component/DaisyNav";
+
+function App() {
+  return <DaisyNav></DaisyNav>;
+}
+
+export default App;
---
+++ src/Component/MyNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyNav = () => {
+  return (
+    <div>
+      <div className="btn btn-ghost">My Nav</div>
+    </div>
+  );
+};
+
+export default MyNav;
