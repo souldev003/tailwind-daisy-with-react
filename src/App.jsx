21018fcdcc63b81135149e0ca11f28b5d63d0a7c
souldev003 <mehrabhosen003@gmail.com>
2026-03-17 18:19:47 +0600

    myNaver almost complete need to show item on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import DaisyNav from "./Component/DaisyNav";
+import MyNav from "./Component/MyNav";
 
 function App() {
-  return <DaisyNav></DaisyNav>;
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <MyNav></MyNav>
+    </>
+  );
 }
 
 export default App;
--- src/Component/MyNav.jsx
+++ src/Component/MyNav.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Circle, CircleX, Menu } from "lucide-react";
 
 const MyNav = () => {
+  const [openStatus, setOpen] = useState(false);
+
   return (
-    <div>
-      <div className="btn btn-ghost">My Nav</div>
+    <div
+      className="flex justify-between items-center p-2.5
+    "
+    >
+      {console.log(openStatus)}
+      <div className="flex items-center gap-3">
+        <div className="ml-3 lg:hidden" onClick={() => setOpen(!openStatus)}>
+          {openStatus ? <CircleX /> : <Menu />}
+        </div>
+        <a className="btn btn-ghost text-xl">My Nav</a>
+      </div>
+      <ul className="hidden lg:flex lg:gap-3">
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      <div>
+        <button className="btn btn-ghost btn-active">Button</button>
+      </div>
     </div>
   );
 };
